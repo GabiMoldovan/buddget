10920fd2d9d69b58c3d5c12035febde9c65ce27f
Gabi Moldovan <68482473+GabiMoldovan@users.noreply.github.com>
2025-07-09 15:34:49 +0300

    initial commit for frontend

---
+++ buddger-frontend/src/App.jsx
@@ -0,0 +1,13 @@
+
+{/* open the terminal and run: npm install packages */ }
+{/* to run the project run: npm run dev     in the terminal and go to your localhost address shown in the terminal*/ }
+
+function App() {
+  return (
+    <>
+      <p>:D</p>
+    </>
+  );
+}
+
+export default App;
