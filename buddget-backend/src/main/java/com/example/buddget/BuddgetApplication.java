1d36afb9c6b8a033100328a067cc6c7c6a240583
Gabi Moldovan <68482473+GabiMoldovan@users.noreply.github.com>
2025-07-09 15:29:00 +0300

    initial commit

---
+++ buddget-backend/src/main/java/com/example/buddget/BuddgetApplication.java
@@ -0,0 +1,13 @@
+package com.example.buddget;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuddgetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuddgetApplication.class, args);
+	}
+
+}
---
+++ buddget-backend/src/test/java/com/example/buddget/BuddgetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.buddget;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuddgetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
